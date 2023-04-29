315cc1bc8dc096cbd74c5fb9c42af2e99341307a
minhlong149 <minhlong9514@gmail.com>
2023-04-29 17:03:32 +0700

    Load comment, ect. on full post view

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
+import postService from '../../services/posts.js';
+
 function ProfilePost() {
   const { state: post } = useLocation();
+  const fullPost = postService.getFullPost(post);
 
   return (
     <section>
       <h2 className='text-xl font-bold'>View the full post</h2>
-      <p>{post.caption}</p>
+      <p>{fullPost.caption}</p>
+      <ul>
+        {fullPost.comments.map((comment) => (
+          <li key={comment.id}>{comment.text}</li>
+        ))}
+      </ul>
     </section>
   );
 }
--- src/services/posts.js
+++ src/services/posts.js
@@ -10,8 +10,25 @@ class PostService {
         caption: 'This is another post',
       },
     ];
-    
+
     return posts;
+  }
+
+  getFullPost(post) {
+    const fullPost = {
+      ...post,
+      comments: [
+        {
+          id: 1,
+          text: 'Since the post on the profile page only has the caption, we need to get the full post from the server.',
+        },
+        {
+          id: 2,
+          text: 'So that we can display the comments as well.',
+        },
+      ],
+    };
+    return fullPost;
   }
 }
 
