b9569ae4e9e3a591e4b5512ecfd4227d24c87f40
Long Nguyen <86862856+minhlong149@users.noreply.github.com>
2023-05-05 17:02:26 +0700

    Setup proxy server (#3)

--- src/components/Notifications/Notifications.jsx
+++ src/components/Notifications/Notifications.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Notifications() {
-  return <section>Notifications</section>;
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    getUser();
+  }, []);
+  return <section>{user.username || 'Ops!'}</section>;
+
+  async function getUser() {
+    const user = await axios.get('api/users/643f55e00459faca4050ec04');
+    setUser(user.data);
+  }
 }
 
 export default Notifications;
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://ie213-social-media-server.cyclic.app',
+        changeOrigin: true,
+      },
+    }
   },
 });
