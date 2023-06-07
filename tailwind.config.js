68b5212bce594680974e49ac0b7b327d5804457b
minhlong149 <minhlong9514@gmail.com>
2023-06-07 16:04:48 +0700

    Styling header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ['Pirata One', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 };
-
