d1bc345e9559a0d6460df65f2d7cd17a38092fc0
minhlong149 <minhlong9514@gmail.com>
2023-06-08 21:00:42 +0700

    Fix form input

--- src/components/Posts/NewPost.jsx
+++ src/components/Posts/NewPost.jsx
@@ -9,36 +9,43 @@ function NewPost({ user }) {
   const handleCreatePost = (e) => {
     e.preventDefault();
     const post = getPostFromForm();
-    console.log(post);
     // postService.createPost(post);
   };
 
   const getPostFromForm = () => {
     // TODO: Get post from the form
     const data = new FormData();
-    data.set('caption', caption);
-    data.set('hashtags', hashtags);
-    data.set('content', content);
-    data.set('mediaURL', mediaURL);
+    data.append('caption', caption);
+    data.append('hashtags', hashtags);
+    data.append('content', content);
+    data.append('mediaURL', mediaURL);
+
+    // Log the data to the console
+    for (let [key, value] of data.entries()) {
+      console.log(`${key}: ${value}`);
+    }
+
     return data;
   };
 
   return (
     <form onSubmit={handleCreatePost}>
       <input
-        type='caption'
+        type='text'
         placeholder={'Caption'}
         value={caption}
         onChange={(ev) => setCaption(ev.target.value)}
       />
       <input
-        type='hashtags'
+        type='text'
         placeholder={'Hashtags'}
         value={hashtags}
         onChange={(ev) => setHashtags(ev.target.value)}
       />
-      <input type='mediaURL' onChange={(ev) => setMediaURL(ev.target.value)} />
-      <button style={{ marginTop: '5px' }}>Create post</button>
+      <input type='file' onChange={(ev) => setMediaURL(ev.target.files[0])} />
+      <button className='bg-sky-500 rounded text-white px-2 py-1' type='submit'>
+        Create post
+      </button>
     </form>
   );
 }
