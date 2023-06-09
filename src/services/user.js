73020423724911191f4636637ca5337c56595d67
vinhlp02 <118278802+vinhlp02@users.noreply.github.com>
2023-06-09 09:49:34 +0700

    Add profile file (#17)
    * Profile update
    
    * Update Profile.jsx
    
    ---------
    
    Co-authored-by: Long Nguyen <86862856+minhlong149@users.noreply.github.com>

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
 class UserService {
+  getUser(user) {
+    return axios.get('/api/users/'+ user._id);
+  }
+
+  getFriends(user) {
+    return axios.get('/api/users/'+ user._id+'/friends');
+  }
+
   getUserByUsername(userId) {
     return axios.get(`/api/users/${userId}`);
   }
